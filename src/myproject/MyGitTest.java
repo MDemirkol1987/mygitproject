79c07ea5778a67f457576ff0bd6ee514cd498224
Mdemirkol1987 <mustafa.demirkol.sn@icloud.com>
2021-04-16 23:09:19 +0300

    hata durumu

--- src/myproject/MyGitTest.java
+++ src/myproject/MyGitTest.java
@@ -13,6 +13,7 @@ public class MyGitTest {
 			}
 			System.out.println(str.substring(0, str.length()-1)+"="+sum);
 			System.out.println("Enis Abi bak bu kodu cok zor yazdim"); 
+			System.out.println("Buraya kod ekledim");
 
 
 	}
