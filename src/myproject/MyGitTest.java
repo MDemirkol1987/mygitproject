eb7ae3743c5b1134384d87725de171128c82d120
Mdemirkol1987 <mustafa.demirkol.sn@icloud.com>
2021-04-16 22:26:41 +0300

    Git Test

---
+++ src/myproject/MyGitTest.java
@@ -0,0 +1,19 @@
+package myproject;
+
+public class MyGitTest {
+
+	public static void main(String[] args) {
+		int sum=0;
+		String str="";
+		for (int j=1;j<7;j++) {
+			sum+=j;
+			str+=j+"+";	
+			if (sum==1) {
+				continue;
+			}
+			System.out.println(str.substring(0, str.length()-1)+"="+sum);
+
+	}
+	}
+
+}
